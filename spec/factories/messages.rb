4a4375adce681c18e2b46c6b0b003745df4b9288
Hiro <Hironori@Apple-no-MacBook-Pro.local>
2017-11-07 17:11:51 +0900

    #WHAT 修正
    ・letを使ってmessageモデルをテストできるようにした。
    ・gemfileのrakeファイルのバージョンのせいで、bundle exec rspecが’通らなかったので直した
    ・factory-girlのgemを使ってテスト用インスタンスを作った

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
 
 	factory :message do
-		body     { Faker::Lorem.sentence }
+		body      "neko"
+		group_id  "1"
+    user_id   "1"
 	end
 
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 describe Message do
+
+  let(:message) { build(:message) }
+
+
   describe '#create' do
     it "is valid with a body, group_id, user_id" do
-      message = build(:message, group_id:"1", user_id: "1")
       expect(message).to be_valid
     end
 
     it "is invalid without a body" do
-      message = build(:message, body: "")
+      message.body = ""
       message.valid?
-      expect(message.errors[:body]).to include("を入力してください。")
+      expect(message.errors[:body]).to include("を入力して下さい。")
     end
 
     it "is invalid without a group_id" do
@@ -24,6 +27,5 @@ describe Message do
       message.valid?
       expect(message.errors[:user_id]).to include("を入力してください。")
     end
-
   end
 end
