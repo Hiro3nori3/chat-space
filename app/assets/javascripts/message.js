206d2faf3ab1337766a3a50f1bbf4ff07d0d01c7
Hiro <Hironori@Apple-no-MacBook-Pro.local>
2018-02-19 15:33:15 +0900

    message.jsの編集
    メッセージをHTMLで描画したり、自動スクロールの機能を追加した

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,86 @@
-$(function(){
-	$('#new_form').on('submit', function(e){
-		e.preventDefault();
-		var formData = new FormData(this);
-		var url =$(this).attr('action')
-		$.ajax({
-			url: url,
-			type: "POST",
-			data: formData,
-			dataType: 'json',
-			processData: false,
-			contentType: false
-		})
-	});
+ $(function() {
+  function buildHTML(message){
+      var image = "";
+      image = `<img src="${message.image.url}" class="large-img">`;
+    if(message.body && message.image.url) {
+      var html = `<ul class="chat-messages">
+                    <div class="rightcontent--content-speaker">
+                    ${message.user_name}
+                    </div>
+                    <div class="rightcontent--content-timestamps">
+                    ${message.day}
+                    </div>
+                    <div class="rightcontent--content-talk">
+                      ${message.body}
+                    </div>
+                    <div class="image">
+                      ${image}
+                    </div>
+                      </ul>`
+      return html;
+    }
+    else if(message.body) {
+      var html = `<ul class="chat-messages">
+                    <div class="rightcontent--content-speaker">
+                    ${message.user_name}
+                    </div>
+                    <div class="rightcontent--content-timestamps">
+                    ${message.day}
+                    </div>
+                    <div class="rightcontent--content-talk">
+                      ${message.body}
+                      </div>
+                      </ul>`
+      return html;
+    }
+    else if(message.image.url) {
+      var html = `<ul class="chat-messages">
+                    <div class="rightcontent--content-speaker">
+                    ${message.user_name}
+                    </div>
+                    <div class="rightcontent--content-timestamps">
+                    ${message.day}
+                    </div>
+                    <div class="image">
+                      ${image}
+                    </div>
+                      </ul>`
+      return html;
+    }
+
+    else {
+      alert('メッセージを入力してください');
+       }
+     }
+
+  $('#new_form').on('submit', function(e){
+    e.preventDefault();
+    console.log(this);
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.rightcontent--content').append(html);
+      $('.rightcontent--footer__body-form').val('');
+      $('.image_form_button').val('');
+      $('.rightcontent--content').animate({scrollTop: $('.rightcontent--content')[0].scrollHeight}, 'fast');
+        $('.rightcontent--footer__button-send').prop("disabled", false);
+ })
+
+    .fail(function(){
+      alert('メッセージを入力してください');
+      $('.rightcontent--footer__button-send').prop("disabled", false);
+    })
+  })
 });
