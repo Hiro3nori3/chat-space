880abc2341e8af9a7963f1623b3a4bd716f94851
Hiro <Hironori@Apple-no-MacBook-Pro.local>
2018-02-21 21:17:54 +0900

    search.jsの編集
    ・変数をキャメルケースで書くようにした
    ・ユーザー追加ボタンを押した際に、そのユーザーがビュー上に描画されるようにした
    ・if文のインデントを調節した

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,20 +1,32 @@
 $(function() {
-	var user_list = $("#user-search-result")
+	var userList = $("#user-search-result")
 
 	function appendUser(user) {
 	 var html = `<div class="chat-group-user clearfix">
 								<p class="chat-group-user__name">${user.name}</p>
 								<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
 							</div>`
-		user_list.append(html)
+		userList.append(html)
 	}
 
 	function appendNoUser(user) {
 		var html = `<li>
 									<div class='chat-group-user clearfix'>${ user }</div>
 								</li>`
-		user_list.append(html);
+		userList.append(html);
 	}
+
+	function appendUserToGroup(user){
+		var user_id = $(user).data("user-id");
+		var user_name = $(user).data("user-name");
+		var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+									<input name='group[user_ids][]' type='hidden' value='${user_id}'>
+									<p class='chat-group-user__name'>${user_name}</p>
+									<a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+								</div>`
+		$("#chat-group-users").append(html);
+	}
+
 
 	$("#user-search-field").on("keyup", function() {
 		var input = $("#user-search-field").val();
@@ -26,16 +38,27 @@ $(function() {
 		})
 		.done(function(users) {
 			$("#user-search-result").empty();
-			if (users.length !== 0) {
-				users.forEach(function(user){
-				appendUser(user);
-			});
-			}else{
-			 appendNoUser("一致するユーザがいません");
-			}
+				if (users.length !== 0) {
+					users.forEach(function(user){
+					appendUser(user);
+				});
+				}	else{
+				 appendNoUser("一致するユーザがいません");
+				}
 		})
 		.fail(function() {
 			alert('ユーザ検索に失敗しました');
 		})
+	});
+	$(document).on('click', '.user-search-add', function(){
+		var user = $(this).parent();
+		user.remove();
+		appendUserToGroup($(this));
+	});
+
+	$(document).on("click",".user-search-remove",function(e){
+		e.preventDefault();
+		var user = $(this).parent();
+		user.remove();
 	});
 });
